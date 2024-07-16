265d544f12a198bb7d55b88d8d8c792384c294fa
KimmyVisscher <kimmy.visscher@student.nhlstenden.com>
2024-07-16 10:25:02 +0200

    added validation in the controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,15 +16,44 @@ class ProductController extends Controller
 
     public function new(Request $request)
     {
-        DB::insert("INSERT INTO products (name, description) VALUES ('$request->name., $request->description')");
+        try {
 
-        return redirect('/products')->with('status', 'Product saved');
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'required|string|max:1000',
+            ]);
+
+            DB::table('products')->insert([
+                'name' => $validatedData['name'],
+                'description' => $validatedData['description']
+            ]);
+
+            return redirect('/products')->with('status', 'Product saved');
+            
+        } catch (\Throwable $th) {
+            \Log::error($th);
+
+            return redirect('/products')->with('error', 'Failed saving the product');
+        }
+        
     }
 
     public function delete(Request $request)
     {
-        DB::delete("DELETE FROM products WHERE id = ".$request->id);
 
-        return redirect('/products')->with('status', 'Product was deleted');
+        try {
+
+            $validatedData = $request->validate([
+                'id' => 'required|integer|exists:products,id',
+            ]);
+    
+            DB::table('products')->where('id', $validatedData['id'])->delete();
+
+            return redirect('/products')->with('status', 'Product was deleted');
+            
+        } catch (\Throwable $th) {
+            \Log::error($th);
+        }
+        
     }
 }
